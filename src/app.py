028617063827501d51330bf6924d42e450a3a33e
JunioResende <junioresende23@gmail.com>
2023-10-14 12:29:51 -0300

    adicionando funcionalidades com callbacks

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, callback, Input, Output
 import plotly.express as px
 
 df = pd.read_csv(
@@ -10,22 +10,48 @@ app = Dash(__name__)
 
 # exibe o dataframe
 app.layout = html.Div([
+    # exibe o grafico em forma de histograma
+    html.H1('Histograma'),
+    html.Hr(),
+    dcc.RadioItems(
+        options=[
+            'pop',
+            'lifeExp',
+            'gdpPercap'
+        ],
+        value='lifeExp',
+        id='radio-controler',
+    ),
+    dcc.Graph(
+        figure={},
+        id='histogram-controller',
+    ),
     html.H1('Exibicao do dataframe'),
     dash_table.DataTable(
         data=df.to_dict('records'),
         page_size=5,
     ),
-    # exibe o grafico em forma de histograma
-    html.H1('Histograma'),
-    dcc.Graph(
-        figure=px.histogram(
-            df,
-            x='continent',
-            y='lifeExp',
-            histfunc='avg',
-        )
-    )
 ])
+
+
+@callback(
+    Output(
+        component_id='histogram-controller',
+        component_property='figure',
+    ),
+    Input(
+        component_id='radio-controler',
+        component_property='value',
+    ),
+)
+def update_graph(col_chosen):
+    fig = px.histogram(
+        df,
+        x='continent',
+        y=col_chosen,
+        histfunc='avg',
+    )
+    return fig
 
 
 # Run the app
